39af855899d022e84fe3d152f9c9dc31d109d230
Alex Casasola <alex@casasola.dev>
2024-01-08 21:30:21 -0800

    Added ellipses to input

--- components/catalyst/input.tsx
+++ components/catalyst/input.tsx
@@ -62,7 +62,7 @@ export const Input = forwardRef<
           ],
 
           // Basic layout
-          'relative block w-full appearance-none rounded-lg px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] sm:px-[calc(theme(spacing[3])-1px)] sm:py-[calc(theme(spacing[1.5])-1px)]',
+          'break-all text-ellipsis relative block w-full appearance-none rounded-lg px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] sm:px-[calc(theme(spacing[3])-1px)] sm:py-[calc(theme(spacing[1.5])-1px)]',
 
           // Typography
           'text-base/6 text-zinc-950 placeholder:text-zinc-500 sm:text-sm/6 dark:text-white',
