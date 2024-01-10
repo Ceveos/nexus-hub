01ae5fdf262297e409082c352953c5fbb00dead7
Alex Casasola <alex@casasola.dev>
2024-01-10 14:58:54 -0800

    Update layout.tsx

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,14 +1,38 @@
+import { Metadata } from "next";
+import { Providers } from "../(providers)/providers";
+import { Analytics } from "@vercel/analytics/react";
+import { cn } from "@/lib/utils";
+
+const title =
+  "Nexus Hub";
+
+export const metadata: Metadata = {
+  title,
+  icons: ["https://nexushub.app/favicon.ico"],
+  openGraph: {
+    title,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    creator: "@Ceveos",
+  },
+  metadataBase: new URL("https://nexushub.app"),
+};
+
 export default function LandingLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <title>Nexus Hub</title>
-      <meta name="description" content="Server administration tool" />
-      <link rel="icon" href="/favicon.ico" />
-      {children}
-    </>
+    <html lang="en" className="h-full" suppressHydrationWarning>
+    <body>
+      <Providers>
+        {children}
+        <Analytics />
+      </Providers>
+    </body>
+  </html>
   );
 }
